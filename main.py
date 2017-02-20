8214cc9a42a2f114fbf0f593c64c41717fa41278
wangshubo1989 <wangshubo1989@126.com>
2017-02-20 17:44:55 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,173 @@
+#-*- coding: UTF-8 -*-
+import webbrowser as web
+import time
+import os
+import random
+count = 400
+j = 0
+
+while j <= count:
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53324586')  #网址自己填入
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/51194426')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50838847')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52506844')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50677164')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52559286')  #网址自己填入
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50529578')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53526891')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50520370')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53895006')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50199599')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53391865')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52752461')  #网
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52927126')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/51813544')  #网址自己填入
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53679718')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52133213')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/49230147')  #网
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53576647')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53884383')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53610359')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/49872889')
+        time.sleep(4)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52507142')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52994327')
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/51975150')
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53437190')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52596855')
+        time.sleep(5)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/49032863')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52523558')
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52576556')
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53144078')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53172689')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/47746317')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53301876')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52904068')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52891464')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52993466')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53290567')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53215877')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/51864556')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53619782')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://mp.weixin.qq.com/s?__biz=MzI0MDYwMDQ5OQ==&mid=2247483651&idx=1&sn=b485e08516e6fc9bc0d8ecaa8508e3dc&chksm=e91915dfde6e9cc9c7f2b9e00e4e0c00df25bccabaab78e79bf55b18ade1ab4a01583e853f3f&scene=0#wechat_redirect')  #网址自己填入
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/48265751')  #网址自己填入
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/51855197')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53203718')
+        time.sleep(1)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/49563001')
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52872058')
+        time.sleep(1)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53340458')  #网址自己填入
+        time.sleep(1)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52627600')
+        time.sleep(3)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53259484')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53187020')
+        time.sleep(1)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53184082')
+        time.sleep(4)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53032477')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53558617')
+        time.sleep(3)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/47023503')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/47019365')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53020757')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53259988')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53007104')
+        time.sleep(2)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52806027')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53471361')
+        time.sleep(3)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52688451')
+        time.sleep(2)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50255263')
+        time.sleep(1)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/54926972')
+        time.sleep(2)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50414529')
+        time.sleep(3)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/48974455')
+        time.sleep(3)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52921285')
+        time.sleep(2)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/48290219')
+        time.sleep(2)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/52029185')
+        time.sleep(1)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53512160')
+        time.sleep(2)
+
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/50179547')
+        time.sleep(2)
+        web.open_new_tab('http://blog.csdn.net/wangshubo1989/article/details/53144603')  #网址自己填入
+        time.sleep(6)
+
+        os.system('taskkill /F /IM chrome.exe')
+        time.sleep(10)
+        j=j+1
